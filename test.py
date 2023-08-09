8ad4cf2f8f18aa22e3168c0f61cb36e6eb273254
Alexa Coupet <alexacoupet@gmail.com>
2023-08-09 15:42:45 -0400

    Wrote 5 tests for the functions/features I implemented: test_logout_route(), test_backup_route(), test_restore_route(), test_search_route(), test_login_route()

--- test.py
+++ test.py
@@ -1,99 +1,93 @@
-from testing.auth_tests import test_hash_password_generates_salt, test_salt_length, test_hash_password_returns_given_salt, test_hash_password_uses_given_salt
-from core.utils import generate_unique_id
-from datetime import datetime
-from testing.db_tests import test_init_db, test_get_inventory_exists, test_dict_factory_link, test_check_connection_threaded
-from testing.core_tests import test_init_sessions, test_add_new_session, test_get_session
+from app import app, sessions
+from app import app, db, sessions, BACKUP_DIR
+import shutil
 import os
-
-# -------- Testing Function Constants --------
-
-AUTH_FUNCS = [test_hash_password_generates_salt,
-              test_salt_length,
-              test_hash_password_returns_given_salt,
-              test_hash_password_uses_given_salt]
-
-DB_FUNCS = [test_init_db, test_get_inventory_exists,
-            test_dict_factory_link,
-            test_check_connection_threaded]
-
-CORE_FUNCS = [test_init_sessions,
-              test_add_new_session,
-              test_get_session]
-
-TESTING_FUNCTIONS = {"core": CORE_FUNCS,
-                     "database": DB_FUNCS,
-                     "authentication": AUTH_FUNCS}
+from datetime import datetime
 
 
-def run_tests(test_type: str, test_funcs: list, report_file_path: str) -> int:
+def test_logout_route():
     """
-    Runs all tests for a given set of test functions.
-
-    args:
-        - test_funcs: a list of test functions to run.
-        - report_file_path: the path to the report file, where the results of the tests will be written.
-
+    Tests the logout route functionality.
+    
     returns:
-        - the number of failed tests as an integer.
-
-    output:
-        - Prints the results of failed tests.
+        - tuple with the first element being True if the test passes and False otherwise, and the second element being a string message indicating the test result.
     """
-    failed_tests = 0
-    with open(report_file_path, "a") as report_file:
-        for test in test_funcs:
-            result, error = test()
-            if not result:
-                report_file.write(f"{error}\n")
-                failed_tests += 1
-        report_file.write(f"{test_type} tests complete.\n")
-        report_file.write(
-            f"{len(test_funcs) - failed_tests} out of {len(test_funcs)} tests passed.\n")
+    # Setting up Flask test client
+    app.config['TESTING'] = True
+    client = app.test_client()
+    
+    # Simulating a logged in user
+    with client.session_transaction() as sess:
+        sess['username'] = 'testuser'
+        # Assuming there's an add_new_session method in your 'sessions' module
+        sessions.add_new_session('testuser')  
 
+    # Call logout route
+    response = client.get('/logout', follow_redirects=True)
 
-def create_report_folder() -> str:
-    """
-    Creates a folder for the test reports.
+    if "You've been logged out!" not in response.data.decode('utf-8'):
+        return False, "Failed: User not logged out successfully."
 
-    args:
-        - None
+    if sessions.is_active('testuser'):  # Assuming there's an is_active method
+        return False, "Failed: User session still active after logout."
 
-    returns:
-        - a unique, identifiable folder path for the reports as a string.
-    """
-    unique_id = generate_unique_id()
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    report_folder_path = f"testing/reports/{current_date}_{unique_id}"
-    os.makedirs(report_folder_path)
-    return report_folder_path
+    response = client.get('/logout', follow_redirects=True)
+    if "You're not logged in!" not in response.data.decode('utf-8'):
+        return False, "Failed: Incorrect message when logging out without being logged in."
 
+    return True, "Passed: Logout route works as expected."
 
-def create_report_file(report_folder_path: str, test_type: str) -> str:
-    """
-    Creates a report file for a given test type.
+def test_backup_route():
+    """Tests the backup functionality."""
+    app.config['TESTING'] = True
+    client = app.test_client()
+    
+    response = client.post('/backup')
+    data = response.get_json()
 
-    args:
-        - report_folder_path: the path to the folder where the reports will be stored.
-        - test_type: the type of tests being run.
+    if data['status'] != "success":
+        return False, "Failed: Backup not successful."
 
-    returns:
-        - the path to the report file as a string.
-    """
-    report_file_path = f"{report_folder_path}/{test_type}_report.txt"
-    with open(report_file_path, "w") as report_file:
-        report_file.write(f"Test report for {test_type} tests.\n")
-    return report_file_path
+    if not os.path.exists(data['message'].split('at ')[-1]):
+        return False, "Failed: Backup file not created."
 
+    return True, "Passed: Backup route works as expected."
 
-def main() -> None:
-    """
-    Runs all tests for the application.
-    """
-    report_folder_path = create_report_folder()
-    for test_type, test_funcs in TESTING_FUNCTIONS.items():
-        report_file_path = create_report_file(report_folder_path, test_type)
-        run_tests(test_type, test_funcs, report_file_path)
+def test_restore_route():
+    """Tests the restore functionality."""
+    app.config['TESTING'] = True
+    client = app.test_client()
 
+    backup_filename = "backup.db"  
+    response = client.post('/restore', data={'backup_filename': backup_filename})
+    data = response.get_json()
 
-if __name__ == "__main__":
-    main()
+    if data['status'] != "success":
+        return False, f"Failed: Restore not successful. Message: {data['message']}"
+
+    return True, "Passed: Restore route works as expected."
+
+def test_search_route():
+    """Tests the search functionality."""
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    query = "apple"  
+    response = client.get(f'/search?q={query}')
+    
+    if "No results found" in response.data.decode('utf-8'):
+        return False, "Failed: Valid product not found."
+
+    return True, "Passed: Search route works as expected."
+
+def test_login_route():
+    """Tests the login functionality."""
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    response = client.post('/home', data={'username': 'testuser', 'password': 'password123'})
+
+    if "Incorrect password for the given username." in response.data.decode('utf-8'):
+        return False, "Failed: Unable to login with valid credentials."
+
+    return True, "Passed: Login route works as expected."

e2b0a1a791054462f36e485dab16951c2f744e69
Snigdha Kola <snigi1234@gmail.com>
2023-08-10 15:38:41 -0400

    Changed fruits to shoes and redid home page style

--- database/starting_data.sql
+++ database/starting_data.sql
@@ -11,13 +11,13 @@ INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_n
 VALUES ('bliskov', '1e4b9ae956cad1385cfa6fffd8323dd16c3fe18c54e6447e49bddef2138d042e84e1505a541c6ef19a5026e684b2559efd366145870a0a8d4d4173c0877f6cd2', 'barbara@thor.com', 'Barbara', 'Liskov');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Apples', 'An edible cultivation of the Malus genus.', 2.00, 100, 'static/images/apple.jpeg', 'Fruit');
+VALUES ('Nike Dunk Low Triple Pink', 'These are the barbie inspired nike dunks. Dance the Night Away!', 150.00, 100, 'static/images/dunks.jpeg', 'Shoes');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Bananas', 'A long curved fruit which grows in clusters and has soft pulpy flesh and yellow skin when ripe.', 1.00, 100, 'static/images/banana.jpeg', 'Fruit');
+VALUES ('Nike Air Cross Trainer 3', 'Flex your sneaker style with these retro orange kicks. ', 220.00, 100, 'static/images/NikeAir.jpeg', 'Shoes');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Mangos', 'The best fruit on the planet.', 4.00, 100, 'static/images/mango.jpeg', 'Fruit');
+VALUES ('Air Jordan 1', 'Still stuck in Asttroworld? Get these Mochas to upgrade to Utopia.', 500.00, 100, 'static/images/Air Jordan 1 .jpeg', 'Shoes');
 
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
 VALUES ('1', 'aturing', '1', 10, '2022-12-21 7:30:30', 5.50);
